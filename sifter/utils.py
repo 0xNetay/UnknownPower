14acec80382404d569b5029240b6fcd784908e78
Nathan <natanx3996@gmail.com>
2020-09-09 19:18:33 +0300

    Fix sifter output

--- sifter/utils.py
+++ sifter/utils.py
@@ -6,7 +6,7 @@ def cstr2py(s):
     return (''.join([chr(x) for x in s])).encode()
 
 def to_hex_string(s):
-    return ''.join([hex(x)[2:] for x in s])
+    return ''.join([hex(x)[2:] for x in s]).zfill(6)
 
 def result_string(insn_hex, result):
     s = "%30s %2d %2d %2d %2d (%s)\n" % (
